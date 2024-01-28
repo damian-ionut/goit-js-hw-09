63ffdb0ce629cb72176a152d18fb2c80596cf614
damian-ionut <143095557+damian-ionut@users.noreply.github.com>
2024-01-28 19:16:37 +0200

    Updates

---
+++ 01-color-switcher.4d2507c6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");t.disabled=!0;const d=document.querySelector("body");let a=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.4d2507c6.js.map
---
+++ 01-color-switcher.6bd5fa21.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.6bd5fa21.js.map
